5d094c4e71c06d865a3243f6236d2220041c69cb
Joy Marocho <joy.marocho@student.moringaschool.com>
2022-04-10 23:39:40 +0300

    adding bootstrap creating a form component adding pipe adding directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
 import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GoalComponent,
-    GoalDetailComponent
+    GoalDetailComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    GoalFormComponent
   ],
   imports: [
     BrowserModule,
--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Goal } from '../goal';
 
 @Component({
@@ -9,8 +9,13 @@ import { Goal } from '../goal';
 export class GoalDetailComponent implements OnInit {
 
   @Input() goal: Goal;
-  constructor() {}
+  @Output() isComplete = new EventEmitter<boolean>();
 
-  ngOnInit(): void {}
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  constructor() { }
+
+  ngOnInit(){ }
 
 }
